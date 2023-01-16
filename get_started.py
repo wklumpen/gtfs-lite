77d2f4fba59354c0a991f7828e51d88183651a50
Willem Klumpenhouwer <willemjjk@gmail.com>
2023-01-16 15:24:24 -0500

    Updating get started.

--- get_started.py
+++ get_started.py
@@ -3,11 +3,15 @@
 from gtfslite import GTFS
 from datetime import date
 
+import pandas as pd
+
 # You can load directly from a zipfile
-feed = GTFS.load_zip("data/mbta_current.zip")
+feed = GTFS.load_zip("data/yyc_gtfs_2022-09-28.zip")
 # feed.set_date(date(2022, 10, 11))
 
 # feed = GTFS.load_zip("metrolinx.zip")
 
 # Print a summary of the data set
-print(feed.stop_times_at_stop(70061, date(2020, 5, 13), start_time='07:00:00'))
+mx = feed.route_frequency_matrix(date(2022, 10, 12), interval=30)
+mx = pd.merge(mx, feed.routes, on='route_id')
+mx.to_csv('calgary_frequency_matrix.csv', index=False)
