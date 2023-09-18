d2a83cf2e8231a676abe717b746ed468dfaed0bf
Willem Klumpenhouwer <willemjjk@gmail.com>
2023-09-18 14:19:10 -0600

    Updates to getting started code.

--- get_started.py
+++ get_started.py
@@ -14,7 +14,7 @@ feed = GTFS.load_zip("data/ttc_gtfs_2022-09-01.zip")
 # print(feed.stop_times.arrival_time.max())
 print(feed.summary())
 # Print a summary of the data set
-mx = feed.route_frequency_matrix(date(2022, 9, 14), interval=60, start_time="06:00", end_time="24:00")
+mx = feed.route_frequency_matrix(date(2022, 9, 14), interval=60, start_time="05:00", end_time="12:00")
 
 mx = pd.merge(mx, feed.routes, on="route_id")
 mx.to_csv("ttc_frequency_matrix.csv", index=False)
